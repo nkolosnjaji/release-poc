df1cee860bacca9e08775eb8b424bd53ffd6bb78
Nikola Kološnjaji <n.kolosnjaji@levi9.com>
2018-06-05 10:50:14 +0200

    Initial commit

---
+++ src/main/java/com/nikkol/ReleasePocApplication.java
@@ -0,0 +1,12 @@
+package com.nikkol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasePocApplication.class, args);
+	}
+}
